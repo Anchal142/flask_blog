a4139453184845ab1457064a8d7a0c59f1020fba
Anchal142 <anchalsingh1029@gmail.com>
2022-06-15 14:11:24 +0530

    commit

--- bootstrap.py
+++ bootstrap.py
@@ -1,7 +1,22 @@
-from flask import Flask,render_template
+import routes as routes
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/http://techmeideas'
+db = SQLAlchemy(app)
 
+
+    # srno name phon_num msg date email
+class Contact(db.Model):
+
+      srno = db.Column(db.Integer, primary_key=True)
+      name = db.Column(db.String(80), nullable=False)
+      phon_num = db.Column(db.String(12), nullable=False)
+      msg = db.Column(db.String(120), nullable=False)
+      date = db.Column(db.String(12),  nullable=False)
+      email = db.Column(db.String(20), nullable=False)
 
 @app.route('/')
 def home():
@@ -13,16 +28,19 @@ def about():
 
 @app.route('/post')
 def post():
-    return render_template("post")
+    return render_template("post.html")
 
-@app.route('/contact')
-def contact():
-    return render_template("contact")
+@app.route('/Contact',methods=['GET','POST'])
+def Contact():
+    if (request.method== 'POST'):
 
+        name= request.form.get("name")
+        email=request.form.get("email")
+        phone=request.form.get("phone")
+        message=request.form.get("message")
+        return render_template("contact.html")
 
-
-
-
-
-
+entry= Contact(name= name,phone_num=phone,msg=message,email= email)
+db.session.add(entry)
+db.session.commit()
 app.run(debug=True)
